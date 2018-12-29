1e1cb78b1881c6e1eac58e6c6b36022089e50222
Michael Ko <mk872@cornell.edu>
2018-12-28 19:36:58 -0500

    Finish heap and heap sort

--- heap/lib/heap.rb
+++ heap/lib/heap.rb
@@ -11,12 +11,19 @@ class BinaryMinHeap
   end
 
   def extract
+    self.store[0], self.store[-1] = self.store[-1], self.store[0]
+    extracted = self.store.pop
+    BinaryMinHeap.heapify_down(self.store, 0)
+    extracted
   end
 
   def peek
+    self.store[0]
   end
 
   def push(val)
+    self.store.push(val)
+    BinaryMinHeap.heapify_up(self.store, self.count - 1)
   end
 
   public
@@ -32,10 +39,34 @@ class BinaryMinHeap
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    prc ||= Proc.new { |el1, el2| el1 <=> el2 }
+    children = child_indices(len, parent_idx)
+
+    return array if children.all? do |child_i|
+      prc.call(array[parent_idx, array[child_i]]) < 1
+    end
+
+    if children.length == 1
+      swap_idx = children[0]
+    elsif prc.call(array[children[0]], array[children[1]]) == -1
+      swap_idx = children[0]
+    else
+      swap_idx = children[1]
+    end
+
+    array[swap_idx], array[parent_idx] = array[parent_idx], array[swap_idx]
+    heapify_down(array, swap_idx, len, &prc)
 
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
-    
+    prc ||= Proc.new {|el1, el2| el1 <=> el2 }
+    return array if child_idx == 0
+    parent = parent_index(child_idx)
+
+    return array if prc.call(array[parent], array[child_idx]) < 1
+
+    array[child_idx], array[parent] = array[parent], array[child_idx]
+    heapify_up(array, parent, len, &prc)
   end
 end
--- heap/lib/heap_sort.rb
+++ heap/lib/heap_sort.rb
@@ -2,5 +2,16 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    (1...length).each do |i|
+      BinaryMinHeap.heapify_up(self, i)
+    end
+
+    length.downto(1).each do |i|
+      self[i - 1], self[0] = self[0], self[i - 1]
+      BinaryMinHeap.heapify_down(self, 0, i - 1)
+    end
+
+    self.reverse!
+
   end
 end
